df0e1d3558cc461d1164af246eefd3c6037a2b52
GGM <ra116843@uem.br>
2021-04-17 17:26:56 -0300

    att

--- main.c
+++ main.c
@@ -23,9 +23,96 @@ int FilaDuplaVazia(struct TpFilaDupla D)
   {
     return 1;
   }
+  return 0;
+}
+
+int FilaDuplaCheia(struct TpFilaDupla D)
+{
+  if (D.Esq == -1 && D.Dir == TamMax)
+  {
+    return 1;
+  }
+  return 0;
+}
+
+struct TpFilaDupla DeslocaFilaDupla(struct TpFilaDupla D)
+{
+  int desloc;
+  if (D.Dir == TamMax)
+  {
+    desloc = (D.Esq + 1) / 2;
+
+    for (int i = D.Esq + 1; i <= D.Dir; i++)
+    {
+      D.Elem[i - desloc] = D.Elem[i];
+    }
+
+    D.Esq = D.Esq - desloc;
+    D.Dir = D.Dir - desloc;
+
+    return D;
+  }
   else
   {
-    return 0;
+    desloc = (TamMax - D.Dir) / 2;
+
+    for (int i = D.Dir; i >= D.Esq + 1; i--)
+    {
+      D.Elem[i + desloc] = D.Elem[i];
+    }
+
+    D.Esq = D.Esq + desloc;
+    D.Dir = D.Dir + desloc;
+
+    return D;
+  }
+}
+
+struct TpFilaDupla InsereDireitaFilaDupla(struct TpFilaDupla D, char x)
+{
+  if (FilaDuplaCheia(D) == 0)
+  {
+    if (D.Dir == TamMax)
+    {
+      D = DeslocaFilaDupla(D);
+    }
+
+    D.Dir = D.Dir + 1;
+    D.Elem[D.Dir] = x;
+
+    return D;
+  }
+}
+
+struct TpFilaDupla RemoveEsquerdaFilaDupla(struct TpFilaDupla D, char x)
+{
+  if (FilaDuplaVazia(D) == 0)
+  {
+    D.Esq = D.Esq + 1;
+    x = D.Elem[D.Esq];
+
+    if (FilaDuplaVazia(D) == 1)
+    {
+      D = InicializaFilaDupla(D);
+    }
+
+    return D;
+  }
+}
+
+struct TpFilaDupla RemoveDireitaFilaDupla(struct TpFilaDupla D, char x)
+{
+  if (FilaDuplaVazia(D) == 0)
+  {
+    x = D.Elem[D.Dir];
+    D.Dir = D.Dir - 1;
+
+    if (FilaDuplaVazia(D) == 1)
+    {
+      D = InicializaFilaDupla(D);
+    }
+
+    return D;
   }
 }
 
@@ -36,7 +123,12 @@ int main()
 
   if (FilaDuplaVazia(D) == 1)
   {
-    printf("ta vazio");
+    printf("ta vazio\n");
+  }
+
+  if (FilaDuplaCheia(D) == 1)
+  {
+    printf("ta cheia");
   }
 
   return 0;
